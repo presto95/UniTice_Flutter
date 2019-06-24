01d69a5605d7f019e572c15612795ea148b0b50b
Presto <yoohan95@gmail.com>
2019-06-25 01:36:56 +0900

    Write web scraping service

--- lib/service/scrap_service.dart
+++ lib/service/scrap_service.dart
@@ -1,23 +1,13 @@
-import "dart:convert";
 import "package:http/http.dart" as http;
 import "package:html/parser.dart";
 import "package:html/dom.dart";
 
 class ScrapService {
+  static ScrapService shared = ScrapService();
 
-  void initiate() async {
-    try {
-      final response = await http.get('https://www.seoultech.ac.kr/service/info/matters/?bidx=6112&bnum=6112&allboard=true&size=5');
-      print(response.body);
-      final document = parse(response.body);
-      final links = document.querySelectorAll("div.wrap_list > tr.body_tr > td");
-      print("--------------");
-      links.forEach((link) {
-        print(link.text);
-      });
-      //print(links);
-    } catch (error) {
-      print(error);
-    }
+  Future<Document> request(String url) async {
+    final response = await http.get(url);
+    final document = parse(response.body);
+    return document;
   }
 }
