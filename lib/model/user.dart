de0d77b01e7582a91c19e23f900b95f56b43bdea
Presto <yoohan95@gmail.com>
2019-06-25 17:30:13 +0900

    Write user model using shared preferences

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,18 +1,21 @@
-import 'package:meta/meta.dart';
-import 'package:unitice/model/post.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class User {
-  /// 현재 선택된 대학교.
-  final String university;
+  static final String _universityKey = "university";
+  static final String _keywordsKey = "keywords";
 
-  /// 현재 등록된 키워드.
-  final List<String> keywords;
+  static Future<SharedPreferences> get preferences =>
+      SharedPreferences.getInstance();
 
-  /// 현재 저장된 북마크.
-  final List<Post> bookmarks;
+  static Future<String> get university async =>
+      (await preferences).getString(_universityKey);
 
-  User(
-      {@required this.university,
-      this.keywords = const <String>[],
-      this.bookmarks = const <Post>[]});
+  static Future<void> setUniversity(String university) async =>
+      (await preferences).setString(_universityKey, university);
+
+  static Future<List<String>> get keywords async =>
+      (await preferences).getStringList(_keywordsKey);
+
+  static Future<void> setKeywords(List<String> keywords) async =>
+      (await preferences).setStringList(_keywordsKey, keywords);
 }
